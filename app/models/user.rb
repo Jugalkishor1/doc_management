af7005c9afc5711a520627ad2201813b0bd8e248
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-06-07 17:04:18 +0530

    Add enum role field to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  enum role: {
+    super_admin: 0,
+    manager: 1,
+    supervisor: 2,
+    data_entry_operator: 3,
+    client: 4
+  }
 end
