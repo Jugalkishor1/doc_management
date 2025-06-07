e8418b542316d0fec8192abc52dd6c24d9ae0763
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-06-07 21:32:38 +0530

    Allow supervisor to view all users except admins and view client documents

--- app/controllers/supervisor/dashboard_controller.rb
+++ app/controllers/supervisor/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class Supervisor::DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_supervisor!
+
   def index
+    @data_entry_operators = current_user.data_entry_operators
+    @clients = User.where(data_entry_operator_id: @data_entry_operators.pluck(:id))
+    @documents = Document.where(user_id: @clients.pluck(:id))
+  end
+
+  private
+
+  def ensure_supervisor!
+    redirect_to root_path unless current_user.supervisor?
   end
 end
