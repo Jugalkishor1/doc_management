0ab1718ee880d39680e087c9b02f60e49e695334
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-06-08 15:41:09 +0530

    Add seed data with default users and role-based relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,34 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.find_or_create_by!(
-  email: "superadmin@example.com",
-  password: "password",
-  role: "super_admin"
-)
+super_admin = User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.password = "password"
+  user.role = :super_admin
+end
+
+manager = User.find_or_create_by!(email: "manager@example.com") do |user|
+  user.password = "password"
+  user.role = :manager
+  user.manager = super_admin
+end
+
+supervisor = User.find_or_create_by!(email: "supervisor@example.com") do |user|
+  user.password = "password"
+  user.role = :supervisor
+  user.manager = manager
+end
+
+deo = User.find_or_create_by!(email: "deo@example.com") do |user|
+  user.password = "password"
+  user.role = :data_entry_operator
+  user.manager = manager
+  user.supervisor = supervisor
+end
+
+client = User.find_or_create_by!(email: "client@example.com") do |user|
+  user.password = "password"
+  user.role = :client
+  user.manager = manager
+  user.supervisor = supervisor
+  user.data_entry_operator = deo
+end
